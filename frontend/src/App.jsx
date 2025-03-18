a593cca0a9dadefcc676224b1b889df555fea313
didi <eljamiyenaa@gmai.com>
2025-03-18 14:07:21 +0000

    Création du composant NavBar et importation des autres components en App. JSX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Home from './Home'
-import AddProject from './AddProject'
-import AddTask from './AddTask'
-import EditProject from './EditProject'
-import EditResource from './EditResource'
-import EditTask from './EditTask'
-import ResourcesList from './ResourcesList'
-import Tasks from './Tasks'
-import TaskResources from './TaskResources'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import AddProject from "./pages/AddProject";
+import AddTask from "./pages/AddTask";
+import EditProject from "./pages/EditProject";
+import EditResource from "./pages/EditResource";
+import EditTask from "./pages/EditTask";
+import ResourcesList from "./pages/ResourcesList";
+import Tasks from "./pages/Tasks";
+// import Projects from "./pages/Projects";
+import TaskResources from "./pages/TaskResources";
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Home/>
-      <AddProject />
-      <AddProject />
-      <AddTask />
-      <EditProject />
-      <EditResource />
-      <EditTask />
-      <ResourcesList />
-      <Tasks />
-      <TaskResources/>
-
-    </>
-
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/add" element={<AddProject />} />
+        <Route path="/tasks/add" element={<AddTask />} />
+        <Route path="/projects/edit/:id" element={<EditProject />} />
+        <Route path="/resources/edit/:id" element={<EditResource />} />
+        <Route path="/tasks/edit/:id" element={<EditTask />} />
+        <Route path="/resources" element={<ResourcesList />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/task-resources" element={<TaskResources />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import { FaHardHat } from "react-icons/fa";
 
-function NavBar() {
+const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <>
-            <div>
-                navigator
-            </div>
-        </>
+        <div className="px-4 py-3">
+            <nav className="bg-[#003f6b] text-white py-5 px-6 shadow-xl rounded-lg border-t-4 border-[#feb913]">
+                <div className="container mx-auto flex justify-between items-center">
+                    <Link to="/" className="text-2xl font-extrabold tracking-tight flex items-center gap-2 group">
+                        <FaHardHat className="text-[#feb913] group-hover:text-[#7acdf1] transition-colors duration-300" size={30} />
+                        <div>
+                            <span className="text-white group-hover:text-[#7acdf1] transition-colors duration-300">Construction</span>
+                            <span className="text-[#feb913] group-hover:text-[#f6821f] transition-colors duration-300">Xpert</span>
+                        </div>
+                    </Link>
 
-    )
-}
+                    <div className="hidden md:flex space-x-8">
+                        <NavLink to="/" label="Home" />
+                    </div>
 
-export default NavBar
+                    <button
+                        className="md:hidden bg-[#dc4048] hover:bg-[#f6821f] p-2 rounded-lg transition-colors duration-200"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        {isOpen ? <X size={24} /> : <Menu size={24} />}
+                    </button>
+                </div>
+
+                {isOpen && (
+                    <div className="md:hidden flex flex-col space-y-4 mt-4 bg-[#003f6b] border-l-4 border-[#dc4048] p-5 rounded-lg">
+                        <NavLink to="/" label="Home" />
+
+                    </div>
+                )}
+            </nav>
+        </div>
+    );
+};
+
+const NavLink = ({ to, label }) => (
+    <Link
+        to={to}
+        className="font-medium relative group"
+    >
+        <span className="text-white hover:text-[#feb913] transition-colors duration-200">{label}</span>
+        <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-[#7acdf1] group-hover:w-full transition-all duration-300 ease-in-out"></span>
+    </Link>
+);
+export default NavBar;
